ff5ee5403fde6feb8ef307e5799b73494ceffcd1
oma coder <omacoder7@gmail.com>
2025-08-01 22:02:06 +0500

    route_fixes

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,10 @@ Route::post('/profile/change-password', [ProfileController::class, 'changePasswo
 Route::get('/profile/add-news', [ProfileController::class, 'showAddNews'])->name('profile.add-news.form');
 Route::post('/profile/add-news', [ProfileController::class, 'addNews'])->name('profile.add-news');
 
-// Админ панель
+
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
     
-    // Управление пользователями
     Route::get('/users', [AdminController::class, 'users'])->name('users');
     Route::get('/users/create', [AdminController::class, 'createUser'])->name('users.create');
     Route::post('/users', [AdminController::class, 'storeUser'])->name('users.store');
@@ -34,11 +33,14 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::put('/users/{id}', [AdminController::class, 'updateUser'])->name('users.update');
     Route::delete('/users/{id}', [AdminController::class, 'deleteUser'])->name('users.delete');
     
-    // Управление новостями
     Route::get('/news', [AdminController::class, 'news'])->name('news');
     Route::get('/news/create', [AdminController::class, 'createNews'])->name('news.create');
     Route::post('/news', [AdminController::class, 'storeNews'])->name('news.store');
     Route::get('/news/{id}/edit', [AdminController::class, 'editNews'])->name('news.edit');
     Route::put('/news/{id}', [AdminController::class, 'updateNews'])->name('news.update');
     Route::delete('/news/{id}', [AdminController::class, 'deleteNews'])->name('news.delete');
+});
+
+Route::get('/', function () {
+    return redirect()->route('news.index');
 });
